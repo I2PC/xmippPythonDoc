e170825c72d03ae12ec932ee62ca9d605df91fc5
biocompwebs <biocompwebs@gmail.com>
2020-08-18 16:14:29 +0200

    buildbot automated-update

---
+++ html/search/classes_4.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['datamanager',['DataManager',['../classdeepConsensusWorkers_1_1deepConsensus__deepLearning1_1_1DataManager.html',1,'deepConsensusWorkers::deepConsensus_deepLearning1']]],
+  ['deeplearningmodel',['DeepLearningModel',['../classdeepDenoising_1_1DeepLearningGeneric_1_1DeepLearningModel.html',1,'deepDenoising::DeepLearningGeneric']]],
+  ['deeptfsupervised',['DeepTFSupervised',['../classdeepConsensusWorkers_1_1deepConsensus__deepLearning1_1_1DeepTFSupervised.html',1,'deepConsensusWorkers::deepConsensus_deepLearning1']]]
+];
