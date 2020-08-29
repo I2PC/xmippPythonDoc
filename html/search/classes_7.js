a5ff61ac8119e618eb2fdd91ba70616c74dc6436
biocompwebs <biocompwebs@gmail.com>
2020-08-29 07:36:48 +0200

    buildbot automated-update

--- html/search/classes_7.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['hashtabledict',['HashTableDict',['../classxmipp3_1_1protocols_1_1protocol__classification__gpuCorr__semi_1_1HashTableDict.html',1,'xmipp3.protocols.protocol_classification_gpuCorr_semi.HashTableDict'],['../classxmipp3_1_1protocols_1_1protocol__classification__gpuCorr__full_1_1HashTableDict.html',1,'xmipp3.protocols.protocol_classification_gpuCorr_full.HashTableDict']]],
-  ['helicalfinder',['HelicalFinder',['../classxmipp3_1_1base_1_1HelicalFinder.html',1,'xmipp3::base']]]
-];
--- html/search/variables_13.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['uniform_5fnoise',['UNIFORM_NOISE',['../classxmipp3_1_1protocols_1_1protocol__preprocess_1_1protocol__add__noise_1_1XmippProtAddNoise.html#a80732c5208e583c121b52f916eef2d53',1,'xmipp3::protocols::protocol_preprocess::protocol_add_noise::XmippProtAddNoise']]],
-  ['url',['url',['../namespacesetup.html#afc13124aa5c0124e84e1d965e3f4b0fb',1,'setup']]],
-  ['usage',['usage',['../namespacepython_1_1vol2sf.html#aa51224ea7951745a1fc15b57427dbb50',1,'python::vol2sf']]],
-  ['use_5fmultiprocessing',['use_multiprocessing',['../namespacedeepResLearner_1_1cnn__deepRes__1__7.html#a974ce9187fe57db88494d2bfe2da4042',1,'deepResLearner.cnn_deepRes_1_7.use_multiprocessing()'],['../namespacedeepResLearner_1_1cnn__deepRes__2__13.html#ad1a29e6ce533d8586bd12edb7910bc24',1,'deepResLearner.cnn_deepRes_2_13.use_multiprocessing()']]],
-  ['useasref',['useAsRef',['../classxmipp3_1_1protocols_1_1protocol__classification__gpuCorr__semi_1_1XmippProtStrGpuCrrSimple.html#a86103c17bc3d54224c9ae6046b1264fb',1,'xmipp3::protocols::protocol_classification_gpuCorr_semi::XmippProtStrGpuCrrSimple']]]
-];
