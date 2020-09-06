faef60b05c7fea8bbfe3e4df856598741835f81f
biocompwebs <biocompwebs@gmail.com>
2020-09-06 16:06:33 +0200

    buildbot automated-update

---
+++ html/search/classes_6.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['gan',['GAN',['../classdeepDenoising_1_1gan_1_1GAN.html',1,'deepDenoising::gan']]],
+  ['generalwedge',['GeneralWedge',['../classpython_1_1tompy_1_1filter_1_1GeneralWedge.html',1,'python::tompy::filter']]],
+  ['gtestresult',['GTestResult',['../classtests_1_1test_1_1GTestResult.html',1,'tests::test']]]
+];
---
+++ html/search/variables_16.js
@@ -0,0 +1,26 @@
+var searchData=
+[
+  ['x',['x',['../classxmipp3_1_1protocols_1_1protocol__validate__fscq_1_1XmippProtValFit.html#a5016a33953a49a8706784fbb8f260442',1,'xmipp3.protocols.protocol_validate_fscq.XmippProtValFit.x()'],['../classxmipp3_1_1viewers_1_1viewer__resolution__fso_1_1XmippProtFSOViewer.html#a570b652f83e778f71730d7d0d1b233fd',1,'xmipp3.viewers.viewer_resolution_fso.XmippProtFSOViewer.x()'],['../classbatch__deepRes__resolution_1_1VolumeManager.html#a8cfaf768d53a0d8ca89cf55938e7f344',1,'batch_deepRes_resolution.VolumeManager.x()'],['../classpython_1_1constrained__frm_1_1FixedAxisConstraint.html#a0d238888ff60848a21b61f3453139ef9',1,'python.constrained_frm.FixedAxisConstraint.x()'],['../classpython_1_1tompy_1_1plot_1_1TomImage.html#a7442797cb3b8234f65d8072f6200c9ed',1,'python.tompy.plot.TomImage.x()'],['../classdeepDenoising_1_1unet_1_1WriteImageCBack.html#ab96dbe2b383fc7a03a6d269e523d2468',1,'deepDenoising.unet.WriteImageCBack.x()']]],
+  ['xdim',['Xdim',['../classxmipp3_1_1protocols_1_1protocol__ransac_1_1XmippProtRansac.html#a89af80506ea44e6227a63dd621488e27',1,'xmipp3.protocols.protocol_ransac.XmippProtRansac.Xdim()'],['../classbatch__deepRes__resolution_1_1VolumeManager.html#afabb1b6c8f5785dcef6d3fc33efce3bb',1,'batch_deepRes_resolution.VolumeManager.Xdim()']]],
+  ['xdim2',['Xdim2',['../classxmipp3_1_1protocols_1_1protocol__ransac_1_1XmippProtRansac.html#a3cc22dc97c2150fcb274fbab18916c9b',1,'xmipp3::protocols::protocol_ransac::XmippProtRansac']]],
+  ['xmipp_5fconda_5fenvs',['XMIPP_CONDA_ENVS',['../namespacexmipp__conda__envs.html#a440fac6e8b64d899f414f601dfb4e353',1,'xmipp_conda_envs']]],
+  ['xmipp_5fcyclic',['XMIPP_CYCLIC',['../namespacexmipp3_1_1constants.html#afa796aa4c03804fb92812ed09612fd71',1,'xmipp3::constants']]],
+  ['xmipp_5fdihedral_5fx',['XMIPP_DIHEDRAL_X',['../namespacexmipp3_1_1constants.html#ad885612f2e265548e8852fd06f59bf50',1,'xmipp3::constants']]],
+  ['xmipp_5fdltk_5fname',['XMIPP_DLTK_NAME',['../namespacexmipp3_1_1constants.html#a2917a54e30868510d85b2a7d10e25bd6',1,'xmipp3::constants']]],
+  ['xmipp_5fhome',['XMIPP_HOME',['../namespacexmipp3_1_1constants.html#ab147566aae9008ee0eadcbb7ec63e060',1,'xmipp3::constants']]],
+  ['xmipp_5fi222',['XMIPP_I222',['../namespacexmipp3_1_1constants.html#ab6760f7e6ce50f4d93e933d466d868cd',1,'xmipp3::constants']]],
+  ['xmipp_5fi222r',['XMIPP_I222r',['../namespacexmipp3_1_1constants.html#ae2c798d60cbcdf4600167bfc2d38dea6',1,'xmipp3::constants']]],
+  ['xmipp_5fin25',['XMIPP_In25',['../namespacexmipp3_1_1constants.html#a5b6e2b9ea99fff35318b6f6cea28a74e',1,'xmipp3::constants']]],
+  ['xmipp_5fin25r',['XMIPP_In25r',['../namespacexmipp3_1_1constants.html#af41ad4c87472565d182375b8b12693b9',1,'xmipp3::constants']]],
+  ['xmipp_5foctahedral',['XMIPP_OCTAHEDRAL',['../namespacexmipp3_1_1constants.html#a95b6a0c4d2e149033026ae007992f7e6',1,'xmipp3::constants']]],
+  ['xmipp_5fsym_5fname',['XMIPP_SYM_NAME',['../namespacexmipp3_1_1constants.html#a1db50fbb5d70eb73c2504e8322db3acc',1,'xmipp3::constants']]],
+  ['xmipp_5ftetrahedral',['XMIPP_TETRAHEDRAL',['../namespacexmipp3_1_1constants.html#aa0bd02da76ab056b1f9dc4c9fb1d7fcc',1,'xmipp3::constants']]],
+  ['xmipp_5fto_5fscipion',['XMIPP_TO_SCIPION',['../namespacexmipp3_1_1constants.html#a3813b28bb5b868cad3eaab3749a68cfc',1,'xmipp3::constants']]],
+  ['xmipp_5furl',['XMIPP_URL',['../namespacexmipp3_1_1constants.html#a2c5a34c4c371825a622b3b1fa4e9e516',1,'xmipp3::constants']]],
+  ['xmippbindir',['xmippBinDir',['../namespacetests_1_1test.html#ac8cffe8add0acdf668f010e614d0c3bc',1,'tests::test']]],
+  ['xmippctf',['xmippCTF',['../classxmipp3_1_1protocols_1_1protocol__ctf__consensus_1_1XmippProtCTFConsensus.html#a44d461e1f94958ba5217246bcaa415c0',1,'xmipp3::protocols::protocol_ctf_consensus::XmippProtCTFConsensus']]],
+  ['xmippdatatest',['xmippDataTest',['../classxmipp3_1_1tests_1_1test__protocol__consensus__classes3D_1_1TestConsensusClasses3D.html#a692f4a36f4929cca0134e86fa15dc2dd',1,'xmipp3::tests::test_protocol_consensus_classes3D::TestConsensusClasses3D']]],
+  ['xml',['xml',['../classtests_1_1test_1_1GTestResult.html#aec2e3659dbc57525a403248874c38f51',1,'tests::test::GTestResult']]],
+  ['xmpi_5frun_5ffile',['xmpi_run_file',['../classxmipp__protocol__preprocess__micrographs__backup_1_1preprocess__A__class.html#a7dc7ace6ff4d50f6fb3c52fc249a2dfd',1,'xmipp_protocol_preprocess_micrographs_backup.preprocess_A_class.xmpi_run_file()'],['../classxmipp__protocol__preprocess__micrographs_1_1preprocess__A__class.html#a5d7d157d1a20ead868d1dca0804747c0',1,'xmipp_protocol_preprocess_micrographs.preprocess_A_class.xmpi_run_file()']]],
+  ['xplotter',['xplotter',['../classxmipp3_1_1viewers_1_1viewer__resolution__fso_1_1XmippProtFSOViewer.html#a8c37be10f1861afd025b7676cbbe55de',1,'xmipp3::viewers::viewer_resolution_fso::XmippProtFSOViewer']]]
+];
