3d72d64a3b00408066e6d7d4b42c4c7f98694a2b
biocompwebs <biocompwebs@gmail.com>
2021-01-15 12:47:08 +0100

    buildbot automated-update

--- html/search/classes_c.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['nmaalignment',['NmaAlignment',['../classtests_1_1test__programs__xmipp_1_1NmaAlignment.html',1,'tests::test_programs_xmipp']]],
-  ['nmaalignmentmpi',['NmaAlignmentMpi',['../classtests_1_1test__programs__xmipp_1_1NmaAlignmentMpi.html',1,'tests::test_programs_xmipp']]]
-];
