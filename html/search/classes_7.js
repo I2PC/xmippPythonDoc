cc5d409c05f3243cab4f79c2afdb8e2485724c0a
biocompwebs <biocompwebs@gmail.com>
2021-01-16 11:43:25 +0100

    buildbot automated-update

--- html/search/classes_7.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['hashtabledict',['HashTableDict',['../classxmipp3_1_1protocols_1_1protocol__classification__gpuCorr__full_1_1HashTableDict.html',1,'xmipp3::protocols::protocol_classification_gpuCorr_full']]],
-  ['hashtabledict',['HashTableDict',['../classxmipp3_1_1protocols_1_1protocol__classification__gpuCorr__semi_1_1HashTableDict.html',1,'xmipp3::protocols::protocol_classification_gpuCorr_semi']]],
-  ['helicalfinder',['HelicalFinder',['../classxmipp3_1_1base_1_1HelicalFinder.html',1,'xmipp3::base']]]
-];
--- html/search/namespaces_2.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['coordinates_5fconsensus',['coordinates_consensus',['../namespacecoordinates__consensus.html',1,'']]],
-  ['coordinatestools',['coordinatesTools',['../namespacecoordinatesTools_1_1coordinatesTools.html',1,'coordinatesTools']]],
-  ['coordinatestools',['coordinatesTools',['../namespacecoordinatesTools.html',1,'']]]
-];
