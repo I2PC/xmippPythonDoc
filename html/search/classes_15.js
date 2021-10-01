5811fb650eafbce4033c2139783f2d664dbb6f05
biocompwebs <biocompwebs@gmail.com>
2021-10-01 15:36:57 +0200

    buildbot automated-update

--- html/search/classes_15.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['wedge',['Wedge',['../classpython_1_1tompy_1_1filter_1_1Wedge.html',1,'python::tompy::filter']]],
-  ['wikibrancher',['WikiBrancher',['../classrelease__docs_1_1WikiBrancher.html',1,'release_docs']]],
-  ['writeimagecback',['WriteImageCBack',['../classdeepDenoising_1_1unet_1_1WriteImageCBack.html',1,'deepDenoising::unet']]]
-];
