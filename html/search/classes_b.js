b430444966d6dbeeb497abffabbac5069ffd46f8
biocompwebs <biocompwebs@gmail.com>
2023-12-19 16:22:28 +0100

    buildbot automated-update

--- html/search/classes_b.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['labeldata',['LabelData',['../classxmipp3_1_1programs_1_1LabelData.html',1,'xmipp3::programs']]],
-  ['localworker',['LocalWorker',['../classgrid_1_1LocalWorker.html',1,'grid']]]
-];
