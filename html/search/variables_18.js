54bb4b17310ddacf9e51d84140bc17deeda0d7bc
biocompwebs <biocompwebs@gmail.com>
2024-01-02 16:18:55 +0100

    buildbot automated-update

--- html/search/variables_18.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['z',['z',['../classxmipp3_1_1protocols_1_1protocol__shift__particles_1_1XmippProtShiftParticles.html#a11c1351dc84def31d591f750872e14e0',1,'xmipp3.protocols.protocol_shift_particles.XmippProtShiftParticles.z()'],['../classxmipp3_1_1protocols_1_1protocol__validate__fscq_1_1XmippProtValFit.html#af62dace419058e38158b3ce8f304835d',1,'xmipp3.protocols.protocol_validate_fscq.XmippProtValFit.z()'],['../classbatch__deepRes__resolution_1_1VolumeManager.html#a3efd582f97c8b7f991c8f81e87e02604',1,'batch_deepRes_resolution.VolumeManager.z()'],['../classpython_1_1constrained__frm_1_1FixedAxisConstraint.html#aa635f9ed945494984394ba0c61652d06',1,'python.constrained_frm.FixedAxisConstraint.z()'],['../classpython_1_1tompy_1_1plot_1_1TomImage.html#a910471aebf00537ecee69c7f9a5a291e',1,'python.tompy.plot.TomImage.z()']]],
-  ['zind',['ZIND',['../classxmipp3_1_1utils_1_1Data.html#a37867e906c614bde008b7c630c1af165',1,'xmipp3::utils::Data']]],
-  ['zones',['zones',['../namespacebatch__deep__global__assignment.html#a74bb22d04203107c706368b824a8e598',1,'batch_deep_global_assignment']]],
-  ['zoom',['zoom',['../classxmipp3_1_1protocols_1_1protocol__classes__2d__mapping_1_1ScatterImageMarker.html#a0624ffc58f42d59891016d4bb39c64fe',1,'xmipp3::protocols::protocol_classes_2d_mapping::ScatterImageMarker']]],
-  ['zscore_5fchoices',['ZSCORE_CHOICES',['../classxmipp3_1_1protocols_1_1protocol__screen__particles_1_1XmippProtScreenParticles.html#a1b2c0d0eeed8f6fb955276651a489596',1,'xmipp3::protocols::protocol_screen_particles::XmippProtScreenParticles']]]
-];
