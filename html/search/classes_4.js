98e45b11220241d2d5013baa75569ee8596f4a6e
biocompwebs <biocompwebs@gmail.com>
2024-02-20 13:56:39 +0100

    buildbot automated-update

--- html/search/classes_4.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['data',['Data',['../classxmipp3_1_1utils_1_1Data.html',1,'xmipp3::utils']]],
-  ['datagenerator',['DataGenerator',['../classbatch__deep__global__assignment_1_1DataGenerator.html',1,'batch_deep_global_assignment.DataGenerator'],['../classbatch__cone__deepalign_1_1DataGenerator.html',1,'batch_cone_deepalign.DataGenerator']]],
-  ['datamanager',['DataManager',['../classdeepConsensusWorkers_1_1deepConsensus__deepLearning1_1_1DataManager.html',1,'deepConsensusWorkers::deepConsensus_deepLearning1']]],
-  ['deeplearningmodel',['DeepLearningModel',['../classdeepDenoising_1_1DeepLearningGeneric_1_1DeepLearningModel.html',1,'deepDenoising::DeepLearningGeneric']]],
-  ['deeptfsupervised',['DeepTFSupervised',['../classdeepConsensusWorkers_1_1deepConsensus__deepLearning1_1_1DeepTFSupervised.html',1,'deepConsensusWorkers::deepConsensus_deepLearning1']]]
-];
