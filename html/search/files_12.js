97f1684e8816883500f95a24df305d6865c3bbed
biocompwebs <biocompwebs@gmail.com>
2024-03-05 13:15:34 +0100

    buildbot automated-update

---
+++ html/search/files_12.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['wizards_2epy',['wizards.py',['../wizards_8py.html',1,'']]],
+  ['write_2epy',['write.py',['../image_2write_8py.html',1,'(Global Namespace)'],['../metadata_2write_8py.html',1,'(Global Namespace)']]]
+];
