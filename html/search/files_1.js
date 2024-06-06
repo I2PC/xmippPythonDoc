8636ca1ab8a86ea743904b24e2323db560356af3
biocompwebs <biocompwebs@gmail.com>
2024-06-06 21:45:43 +0200

    buildbot automated-update

--- html/search/files_1.js
+++
@@ -1,9 +0,0 @@
-var searchData=
-[
-  ['affine_5f2d_2epy',['affine_2d.py',['../affine__2d_8py.html',1,'']]],
-  ['affine_5fmatrix_5f2d_2epy',['affine_matrix_2d.py',['../affine__matrix__2d_8py.html',1,'']]],
-  ['aligned_5f2d_5fclassficiation_2epy',['aligned_2d_classficiation.py',['../aligned__2d__classficiation_8py.html',1,'']]],
-  ['all_5ftests_2epy',['all_tests.py',['../all__tests_8py.html',1,'']]],
-  ['augmentators_2epy',['augmentators.py',['../augmentators_8py.html',1,'']]],
-  ['autoconfig_2epy',['AutoConfig.py',['../install_2scons-tools_2AutoConfig_8py.html',1,'(Global Namespace)'],['../ore_2install_2scons-tools_2AutoConfig_8py.html',1,'(Global Namespace)'],['../iz_2install_2scons-tools_2AutoConfig_8py.html',1,'(Global Namespace)']]]
-];
--- html/search/functions_11.js
+++
@@ -1,9 +0,0 @@
-var searchData=
-[
-  ['qualifyparticles',['qualifyParticles',['../classxmipp3_1_1protocols_1_1protocol__reconstruct__highres_1_1XmippProtReconstructHighRes.html#a792b05452947153262c67e78e3921fb5',1,'xmipp3::protocols::protocol_reconstruct_highres::XmippProtReconstructHighRes']]],
-  ['quaternion_5fconj',['quaternion_conj',['../namespaceswiftalign_1_1transform_1_1quaternion__arithmetic.html#a77597689e52772feac3dae80e3db0ada',1,'swiftalign::transform::quaternion_arithmetic']]],
-  ['quaternion_5ffrom_5fmatrix',['quaternion_from_matrix',['../namespacebatch__deep__global__assignment__predict.html#a2dcacfc41595551526ea8fb9c214d891',1,'batch_deep_global_assignment_predict']]],
-  ['quaternion_5fmatrix',['quaternion_matrix',['../namespacebatch__deep__global__assignment__predict.html#accd32073418b8509246ea94fa033b960',1,'batch_deep_global_assignment_predict']]],
-  ['quaternion_5fproduct',['quaternion_product',['../namespaceswiftalign_1_1transform_1_1quaternion__arithmetic.html#a0258d26e039f9406b1462873cfe6d498',1,'swiftalign::transform::quaternion_arithmetic']]],
-  ['quaternion_5fto_5fmatrix',['quaternion_to_matrix',['../namespaceswiftalign_1_1transform_1_1quaternion__to__matrix.html#a51eb3564f49316acc91caeae7e701f2f',1,'swiftalign::transform::quaternion_to_matrix']]]
-];
