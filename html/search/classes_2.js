40a179cfafd1f467f0f3217047fdcc47fee61127
biocompwebs <biocompwebs@gmail.com>
2024-06-18 13:00:58 +0200

    buildbot automated-update

--- html/search/classes_2.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['bfactorwindow',['BfactorWindow',['../classxmipp3_1_1viewers_1_1viewer__resolution3d_1_1BfactorWindow.html',1,'xmipp3::viewers::viewer_resolution3d']]],
-  ['bnbgpu',['BnBgpu',['../classclassifyPcaFuntion_1_1bnb__gpu_1_1BnBgpu.html',1,'classifyPcaFuntion::bnb_gpu']]]
-];
