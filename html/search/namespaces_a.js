0d1bba2700b6d51b4b3f45e5846869c6cd29cf89
biocompwebs <biocompwebs@gmail.com>
2024-08-01 00:15:59 +0200

    buildbot automated-update

---
+++ html/search/namespaces_a.js
@@ -0,0 +1,17 @@
+var searchData=
+[
+  ['all_5ftests',['all_tests',['../namespacetests_1_1all__tests.html',1,'tests']]],
+  ['test',['test',['../namespacetests_1_1test.html',1,'tests']]],
+  ['test_5fbinding',['test_binding',['../namespacetests_1_1test__binding.html',1,'tests']]],
+  ['test_5fprograms',['test_programs',['../namespacetests_1_1test__programs.html',1,'tests']]],
+  ['test_5fprotocol_5fdeep_5fdenoising',['test_protocol_deep_denoising',['../namespacetest__protocol__deep__denoising.html',1,'']]],
+  ['test_5fprotocols_5fgpucorr_5fclassifier',['test_protocols_gpuCorr_classifier',['../namespacetest__protocols__gpuCorr__classifier.html',1,'']]],
+  ['test_5fprotocols_5fgpucorr_5ffullstreaming',['test_protocols_gpuCorr_fullStreaming',['../namespacetest__protocols__gpuCorr__fullStreaming.html',1,'']]],
+  ['test_5fprotocols_5fgpucorr_5fsemistreaming',['test_protocols_gpuCorr_semiStreaming',['../namespacetest__protocols__gpuCorr__semiStreaming.html',1,'']]],
+  ['test_5fprotocols_5fmetaprotocol_5fgolden_5fhighres',['test_protocols_metaprotocol_golden_highres',['../namespacetest__protocols__metaprotocol__golden__highres.html',1,'']]],
+  ['test_5fprotocols_5fmetaprotocol_5fheterogeneity',['test_protocols_metaprotocol_heterogeneity',['../namespacetest__protocols__metaprotocol__heterogeneity.html',1,'']]],
+  ['test_5fprotocols_5fsolid_5fangles',['test_protocols_solid_angles',['../namespacetest__protocols__solid__angles.html',1,'']]],
+  ['test_5fprotocols_5fsubtract_5fprojection',['test_protocols_subtract_projection',['../namespacetest__protocols__subtract__projection.html',1,'']]],
+  ['tests',['tests',['../namespacetests.html',1,'']]],
+  ['tomogram_5freconstruction',['tomogram_reconstruction',['../namespacetomogram__reconstruction.html',1,'']]]
+];
---
+++ html/search/variables_a.js
@@ -0,0 +1,13 @@
+var searchData=
+[
+  ['k',['k',['../namespacebatch__deep__global__assignment__predict.html#ac3450657826be2b5fc656abed5d687b8',1,'batch_deep_global_assignment_predict']]],
+  ['keep_5falpha',['keep_alpha',['../classbatch__pdb__select_1_1ScriptPDBSelect.html#afe729ae40a83382aff8811e795d5dcb5',1,'batch_pdb_select::ScriptPDBSelect']]],
+  ['keep_5fbeta',['keep_beta',['../classbatch__pdb__select_1_1ScriptPDBSelect.html#a379df6f18df0acb66238ece7d0351021',1,'batch_pdb_select::ScriptPDBSelect']]],
+  ['kernel_5fbspline',['KERNEL_BSPLINE',['../namespacexmipp3_1_1constants.html#a628d4dc5d55c0c96c6166b66663fdaab',1,'xmipp3::constants']]],
+  ['kernel_5flinear',['KERNEL_LINEAR',['../namespacexmipp3_1_1constants.html#a554602fcac8e039c83992416d9271bcb',1,'xmipp3::constants']]],
+  ['kernel_5fnearest',['KERNEL_NEAREST',['../namespacexmipp3_1_1constants.html#a9ac1773f3f64b237392c458ea71cfe3a',1,'xmipp3::constants']]],
+  ['kernel_5ftype',['kernel_type',['../classlibsvm_1_1svm_1_1svm__parameter.html#ab381d5266b617c15195d295d6178a160',1,'libsvm::svm::svm_parameter']]],
+  ['keywords',['keywords',['../classxmipp3_1_1programs_1_1ProgramKeywordsRank.html#abf17a781ded4709024fc43a4aa5ec47a',1,'xmipp3.programs.ProgramKeywordsRank.keywords()'],['../classbatch__apropos_1_1ScriptApropos.html#a2bb1ab5aa35256e5fe54c7ec51a7852e',1,'batch_apropos.ScriptApropos.keywords()'],['../namespacesetup.html#a73ae9ecb109f0dcab6f0b6a89043c5c3',1,'setup.keywords()']]],
+  ['kksqlite',['kksqlite',['../classtest__protocols__subtract__projection_1_1TestSubProj.html#a25942ec50bfeb0fd76270cbf9c0f9a93',1,'test_protocols_subtract_projection::TestSubProj']]],
+  ['kwargs_5ffor_5fextension',['kwargs_for_extension',['../namespacesetup.html#a97826fc45dfdc59ad0623cc312d5de6d',1,'setup']]]
+];
